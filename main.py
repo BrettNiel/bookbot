9b50b95bda23ff70c175a15403fc5f9768edfbbe
BrettNiel <bmniel13@gmail.com>
2025-06-05 22:06:26 -0400

    sorted counts, filtered characters, formatted text

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
 from stats import num_of_words
 from stats import count_of_each_character
+from stats import sorted_dict
+
+book = "books/frankenstein.txt"
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,8 +11,13 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    # print((get_book_text("books/frankenstein.txt")))
-    print(f"{num_of_words(get_book_text("books/frankenstein.txt"))} words found in the document")
-    print(count_of_each_character(get_book_text("books/frankenstein.txt")))
-    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_of_words(get_book_text(book))} total words")
+    print("--------- Character Count -------")
+    for item in sorted_dict(count_of_each_character(get_book_text(book))):
+         if item['char'].isalpha():
+             print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 main()
