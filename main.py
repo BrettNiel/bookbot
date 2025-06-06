928ae8198009d12b93eca836045a7bca24ca5297
BrettNiel <bmniel13@gmail.com>
2025-06-05 22:59:03 -0400

    allowed book input

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from stats import num_of_words
 from stats import count_of_each_character
 from stats import sorted_dict
 
-book = "books/frankenstein.txt"
-
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
@@ -12,11 +10,15 @@ def get_book_text(filepath):
 
 def main():
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book}")
+    import sys
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    print(f"Analyzing book found at {sys.argv[1]}")
     print("----------- Word Count ----------")
-    print(f"Found {num_of_words(get_book_text(book))} total words")
+    print(f"Found {num_of_words(get_book_text(sys.argv[1]))} total words")
     print("--------- Character Count -------")
-    for item in sorted_dict(count_of_each_character(get_book_text(book))):
+    for item in sorted_dict(count_of_each_character(get_book_text(sys.argv[1]))):
          if item['char'].isalpha():
              print(f"{item['char']}: {item['num']}")
     print("============= END ===============")
